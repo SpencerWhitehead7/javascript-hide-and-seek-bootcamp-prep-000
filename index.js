946450adfd23246f6c2649887e3674b62faaa3ec
Spencer Whitehead <spencerwhitehead7@gmail.com>
2018-01-23 21:59:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n){
+  var rankedLists = document.querySelectorAll('.ranked-list')
+  for(var i=0; i<rankedLists.length; i++){
+    rankedLists[i].innerHTML = parseInt(rankedLists[i])+n
+  }
+}
+
+function deepestChild(){
+  var list = document.querySelectorAll('#grand-node div')
+      return list[(list.length-1)]
+}
+
+//function deepestChild3(){
+//  var item = document.querySelector("#grand-node")
+//  var list = document.querySelectorAll('#grand-node div')
+//  for(var i=0; i<list.length; i++){
+//    item = item.querySelector('div')
+//    if(i == list.length-1){
+//      return item
+//    }
+//  }
+//}
