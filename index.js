e0c9a38d4197b6a2bfc41b096ec9cd2014c43ca6
Spencer Whitehead <spencerwhitehead7@gmail.com>
2018-01-23 21:46:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n){
+  var rankedLists = document.querySelectorAll('.ranked-list')
+  for(var i=0; i<rankedLists.length; i++){
+    rankedLists[i].innerHTML = parseInt(rankedLists[i])+n
+  }
+}
+
+function deepestChild(){
+  var list = document.querySelector('#grand-node')
+      return list[(list.length-1)]
+}
+
+function deepestChild2(){
+  var list = document.querySelector("#grand-node")
+  for(var i=0; i<list.length; i++){
+    if(typeof list[i].firstChild === "string"){
+      return list[i]
+    }
+  }
+}
+
+function deepestChild3(){
+  var item = document.querySelector("#grand-node")
+  var list = document.querySelectorAll('#grand-node div')
+  for(var i=0; i<list.length; i++){
+    item = item.querySelector('div')
+    if(){
+      return item
+    }
+  }
+}
